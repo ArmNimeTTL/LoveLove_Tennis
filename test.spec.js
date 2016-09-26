de0238a951fcf71466f7496262946c364c6686b9
Siriwut Netwichian <oiltooil@hotmail.com>
2016-09-26 20:17:04 +0700

    Edit code

--- test.spec.js
+++ test.spec.js
@@ -1,34 +1,16 @@
-function FizzBuzz(number){
-  var str = " ";
-  if(number%15 == 0)
-    return 'FizzBuzz';
-  else if(number%5 == 0)
-    return 'Buzz';
-  else if(number%3 == 0)
-    return 'Fizz';
-  else {
-    return str = tostring.number().str;
+function Tennis(){
+  this.start = funtion(){
+
+  };
+  this.echo = function(){
+    return 'Love-Love';
   }
 }
 
-describe("FizzBuzz()", function() {
-  it('Shold be "Fizz" when I put 3 ' , function(){
-  var str = FizzBuzz(3);
-  expect(str).toEqual('Fizz');
-  });
-
-  it('Shold be "Buzz" when I put 5 ' , function(){
-  var str = FizzBuzz(5);
-  expect(str).toEqual('Buzz');
-  });
-
-  it('Shold be "FizzBuzz" when I put 15 ' , function(){
-  var str = FizzBuzz(15);
-  expect(str).toEqual('FizzBuzz');
-  });
-
-  it('Shold be "4" when I put 4 ' , function(){
-  var str = FizzBuzz(4);
-  expect(str).toEqual('4');
-  });
-});
+describe('Tennis game' , function(){
+  it('should echo "Love - Love" when start the game',function()){
+    var tennis = new tennis();
+    tennis.start();
+    expect(tennis.echo()).toEqual('Love - Love');
+  }
+}
