53f7d8c311adef822bea0ae947333f5fde139191
Siriwut Netwichian <oiltooil@hotmail.com>
2016-09-28 21:12:12 +0700

    Edit code and runner test Love-Love

--- spec/testTennis.spec.js
+++ spec/testTennis.spec.js
@@ -1,9 +1,23 @@
 function Tennis(){
+  var ScoreA;
+  var ScoreB;
   this.start = function(){
+    ScoreA = 0;
+    ScoreB = 0;
+    this.GetScoreA = function(){
+      if(ScoreA === 0) ScoreA = 15;
+      else if(ScoreA === 15) ScoreA = 30;
+      else if(ScoreA === 30) ScoreA = 40;
+    }
+    this.GetScoreB = function(){
+      if(ScoreB === 0) ScoreB = 15;
+      else if(ScoreB === 15) ScoreB = 30;
+      else if(ScoreB === 30) ScoreB = 40;
+    }
   };
 
   this.echo = function(){
-    return 'Love - Love';
+    if(ScoreA===0 && ScoreB ===0) return 'Love-Love';
   }
 }
 
